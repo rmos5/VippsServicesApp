13bdfa99a8e086726dd0a4a6d521db14815a807b
Raivo Mosona <raivo.mosona@pharmadata.fi>
2023-11-12 20:27:14 +0200

    Debug log level update.

--- App.DI.xaml.cs
+++ App.DI.xaml.cs
@@ -21,7 +21,7 @@ namespace VippsServicesApp
         {
             loggerConfiguration.ReadFrom.Configuration(host.Configuration);
             string debugOutputTemplate = "[{Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
-            loggerConfiguration.WriteTo.Debug(outputTemplate: debugOutputTemplate);
+            loggerConfiguration.WriteTo.Debug(restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose, outputTemplate: debugOutputTemplate);
             //note: preserved as a sample configuration code
             //loggerConfiguration.WriteTo.Logger(lc => lc
             //  .Filter.ByIncludingOnly(Matching.FromSource<App>())
